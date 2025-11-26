ae7e58e17b338266ad37b22b32c2b802aa971e29
Hoang Tran <narfmcfarf@gmail.com>
2025-11-26 09:32:02 -0800

    base version of FunFactApp

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My fun fact: ");
+        System.out.println("TODO: replace this text in a Git branch.");
+    }
+}
